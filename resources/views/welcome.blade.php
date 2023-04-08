9d90c03434b32b68852a68519192f5097cf45122
korrio <iamnotkorr@gmail.com>
2023-04-08 13:30:01 +0700

    init play forum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="/favicon.ico">
-    <title>Libeyondea Forum Api</title>
+    <title>Social Bureau Inspector Forum API</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Styles -->
